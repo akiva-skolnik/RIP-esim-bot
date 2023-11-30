52053a23e6b85007c0665a94c8bf3f2abcdf2c08
akiva0003 <akiva0003@gmail.com>
2023-11-30 22:05:10 +0200

    Fixed CONSTRAINT error

--- Help/db_utils.py
+++ Help/db_utils.py
@@ -51,7 +51,8 @@ async def insert_into_api_battles(server: str, battle_id: int) -> dict:
     filtered_api_battles = {k: api_battles[k] for k in api_battles_columns}
 
     placeholders = ', '.join(['%s'] * len(filtered_api_battles))
-    query = f"REPLACE INTO {server}.apiBattles VALUES ({placeholders})"
+    query = f"INSERT INTO {server}.apiBattles VALUES ({placeholders}) " \
+            f"ON DUPLICATE KEY UPDATE {', '.join([f'{k} = VALUES({k})' for k in filtered_api_battles if k != 'battle_id'])}"
     await execute_query(bot.pool, query, tuple(filtered_api_battles.values()))
 
     return filtered_api_battles
